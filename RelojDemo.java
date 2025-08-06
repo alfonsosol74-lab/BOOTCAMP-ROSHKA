bfc9f77472fdaac5e536088b91d8632e37b64cf1
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-08-06 16:53:57 -0300

    RelojDemo.java cambiado
    Muestra la hora del reloj creado antes de aumentar 10 segundos.
    Muestra la hora que se esta restando al reloj orginal.

--- RelojDemo.java
+++ RelojDemo.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+
 public class RelojDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese segundos desde medianoche: ");
         int segundos = scanner.nextInt();
         Reloj reloj1 = new Reloj(segundos);
+        System.out.println("Hora del reloj: " + reloj1.toString());
         System.out.println("Aumentando el reloj 10 veces: ");
         for (int a = 0; a < 10; a++) {
             reloj1.tick();
@@ -14,7 +16,10 @@ public class RelojDemo {
         int segundos2 = scanner.nextInt();
         Reloj reloj2 = new Reloj(segundos2);
         int diferencia = reloj1.restaReloj(reloj2);
+
+        System.out.println("Restando " + reloj1.toString() + " de " + reloj2.toString());
         System.out.println("La diferencia en segundos entre el primer reloj y el segundo reloj es: " + diferencia);
+
         scanner.close();
     }
 }
